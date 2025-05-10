42212b8a7989d2e8b5344963fe066950ba88c29a
dcsarmistha <sarmisthadc2@gmail.com>
2025-05-10 09:09:56 +0545

    now the jokes work

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express= require("express");
 const axios= require("axios");
-const app= 3000;
+const port= 3000;
+const app= express();
+app.set("view engine", 'ejs');
+app.get("/", async(req, res)=>{
+    try{
+    const response= await axios.get("https://v2.jokeapi.dev/joke/Any?type=single");
+    const joke= response.data.joke;
+    res.render("index", {joke});
+    }
+    catch(error){
+        res.render("index.js",{joke: "sorry couldnot find the joke"});
+    }
+});
 
-app.get("/", (req, res)=>{
-    const result= async axios.get("https://v2.jokeapi.dev/joke/Any?type=single")
-})
-
-
-
-
-
-app.listen(prompt, ()=>{
+app.listen(port, ()=>{
     console.log("sever running on the port 3000");
-})
+});
